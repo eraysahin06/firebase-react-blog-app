604664ccd2de6242f061bbdd4c233e84a2990a78
Eray Sahin <eraysahinmoon@gmail.com>
2023-08-25 19:59:00 -0400

    create-post functionality added

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,19 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, auth } from '../firebase-config';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
+  const [title, setTitle] = useState('');
+  const [postText, setPostText] = useState('');
+
+  const postsCollectionRef = collection(db, 'posts');
+  let navigate = useNavigate();
+  const createPost = async () => {
+    await addDoc(postsCollectionRef, {
+      title,
+      postText,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid },
+    });
+    navigate('/');
+  };
+
   return (
     <div className="createPostPage">
       <div className="cpContainer">
         <h1>Create a Post</h1>
         <div className="inputGp">
           <label> Title:</label>
-          <input placeholder="Title Here" />
+          <input
+            placeholder="Title Here"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
         </div>
         <div className="inputGp">
           <label>Post:</label>
-          <textarea placeholder="Blog Post Here"></textarea>
+          <textarea
+            placeholder="Blog Post Here"
+            onChange={(e) => {
+              setPostText(e.target.value);
+            }}
+          ></textarea>
         </div>
-        <button>Submit Post</button>
+        <button onClick={createPost}>Submit Post</button>
       </div>
     </div>
   );
