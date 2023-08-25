2ff8028a2037478551a7a93c41dcd8563dc8b823
Eray Sahin <eraysahinmoon@gmail.com>
2023-08-25 19:44:29 -0400

    create-post done

--- src/pages/CreatePost.js
+++ src/pages/CreatePost.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const CreatePost = () => {
-  return <div>Post</div>;
+  return (
+    <div className="createPostPage">
+      <div className="cpContainer">
+        <h1>Create a Post</h1>
+        <div className="inputGp">
+          <label> Title:</label>
+          <input placeholder="Title Here" />
+        </div>
+        <div className="inputGp">
+          <label>Post:</label>
+          <textarea placeholder="Blog Post Here"></textarea>
+        </div>
+        <button>Submit Post</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
