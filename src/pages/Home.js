fdda173170bfd8915987ea400b53249e99e26187
Eray Sahin <eraysahinmoon@gmail.com>
2023-08-25 20:31:19 -0400

    home page displays all posts

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../firebase-config';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [postLists, setPostLists] = useState([]);
+  const postsCollectionRef = collection(db, 'posts');
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostLists(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{post.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer">{post.postText}</div>
+            <h3>@{post.author?.name}</h3>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Home;
